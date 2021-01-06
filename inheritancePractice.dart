cb98e6981dbd4ade4b320d67fdd336a3c294447c
Hans Herrera <hanschristian.herrera@wvsu.edu.ph>
2021-01-06 16:27:18 +0800

    Added child class Rocket inherited from class Spacecraft

--- inheritancePractice.dart
+++ inheritancePractice.dart
@@ -1,9 +1,14 @@
 void main(List<String> args) {
 
   DateTime mySpacecraftLaunchDate = new DateTime(1995, 04, 22);
-  Spacecraft mySpacecraft = new Spacecraft("International Space Station", mySpacecraftLaunchDate, 100);
+  Spacecraft mySpacecraft = new Spacecraft.launched("International Space Station", mySpacecraftLaunchDate, 100);
 
   mySpacecraft.showSpacecraftData();
+
+  print('');
+
+  Rocket myRocket = new Rocket.unlaunched("Endeavor", 105, "Plasma Rocket");
+  myRocket.showSpacecraftData();
 }
 
 class Spacecraft{
@@ -12,7 +17,11 @@ class Spacecraft{
   DateTime launchDate;
   double totalNetWeight;
 
-  Spacecraft(String name, DateTime launchDate, double totalNetWeight){
+  Spacecraft(){
+
+  }
+
+  Spacecraft.launched(String name, DateTime launchDate, double totalNetWeight){
     this.name = name;
     this.launchDate = launchDate;
     this.totalNetWeight = totalNetWeight;
@@ -65,5 +74,55 @@ class Spacecraft{
       print("Unlaunched");;
     }
     print("Total Net Weight: ${getTotalNetWeight()} tons");
+  }
+}
+
+class Rocket extends Spacecraft{
+
+  String name;
+  DateTime launchDate;
+  double totalNetWeight;
+  String rocketType;
+
+  Rocket(){
+    
+  }
+
+  Rocket.launched(String name, DateTime launchDate, double totalNetWeight, String rocketType){
+    this.name = name;
+    this.launchDate = launchDate;
+    this.totalNetWeight = totalNetWeight;
+    this.rocketType = rocketType;
+  }
+
+  Rocket.unlaunched(String name, double totalNetWeight, String rocketType){
+    this.name = name;
+    this.launchDate = null;
+    this.totalNetWeight = totalNetWeight;
+    this.rocketType = rocketType;
+  }
+
+  void setRocketType(String rocketType){
+    this.rocketType = rocketType;
+  }
+
+  String getRocketType(){
+    return rocketType;
+  }
+
+  void showSpacecraftData(){
+
+    print("Rocket: $name");
+    if(launchDate != null)
+    {
+      int years = DateTime.now().difference(launchDate).inDays ~/ 365;
+
+      print("Launched: ${getLaunchYear()} ($years years ago)");
+    }
+    else{
+      print("Unlaunched");;
+    }
+    print("Total Net Weight: ${getTotalNetWeight()} tons");
+    print("Rocket type: $rocketType");
   }
 }
